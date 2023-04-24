f72855a502de73952e59af8b28105646e939dc34
sun-diane Sun <dianesun1@gmail.com>
2023-04-24 22:15:20 +0000

    First Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def table_directors
+    render({ :template => "director_templates/table_directors"})
+  end
+
+  # def youngest_director
+  # end 
+
+  # def oldest_director
+  # end 
+
+  # def table_actors
+  # end
+  
+  # def table_movies
+  # end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "application", :action => "table_directors" })
+  get("/directors/youngest", { :controller => "application", :action => "youngest_director" })
+  get("/directors/eldest", { :controller => "application", :action => "oldest_director" })
+  
+  get("/actors", { :controller => "application", :action => "table_actors"})
+  get("/movies", { :controller => "application", :action => "table_movies" })
+  
+
 end
